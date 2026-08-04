4ceec32429f333ec245bb10f9f6fc91cb356aca0
budha.jethava <budha.jethava@spec-india.com>
2026-08-04 16:17:43 +0530

    Updated deployment file

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,6 +2,18 @@ output "beanstalk_environment" {
   value = aws_elastic_beanstalk_environment.prod.endpoint_url
 }
 
-output "database_endpoint" {
+output "db_endpoint" {
   value = aws_db_instance.mysql.address
+}
+
+output "db_port" {
+  value = aws_db_instance.mysql.port
+}
+
+output "db_name" {
+  value = aws_db_instance.mysql.db_name
+}
+
+output "db_username" {
+  value = aws_db_instance.mysql.username
 }
