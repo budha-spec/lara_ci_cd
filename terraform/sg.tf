fbb8fc06360dbea96823130ff5bb907040745a8b
budha.jethava <budha.jethava@spec-india.com>
2026-08-05 11:31:37 +0530

    Updated terraform configuration

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -1,40 +1,7 @@
-resource "aws_security_group" "alb" {
-  name   = "${local.name}-alb-sg"
-  vpc_id = aws_vpc.main.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_security_group" "eb" {
-  name   = "${local.name}-eb-sg"
-  vpc_id = aws_vpc.main.id
-
-  ingress {
-    description     = "HTTP from ALB"
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    security_groups = [aws_security_group.alb.id]
-  }
+  name        = "${local.name}-eb-sg"
+  description = "Elastic Beanstalk EC2"
+  vpc_id      = aws_vpc.main.id
 
   egress {
     from_port   = 0
